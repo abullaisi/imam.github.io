4f51bd1c17ac3d3460357ec25c9d82df1952492a
abullaisi <iabullaisi@gmail.com>
2023-04-17 14:55:56 +0700

    Create main.js

---
+++ main.js
@@ -0,0 +1,7 @@
+  /**
+   * delete loom
+   */
+  document.addEventListener('scroll', () => {
+    const loomSection = document.querySelector('#loom-companion-mv3')
+    loomSection.style.display = 'none'
+  })
